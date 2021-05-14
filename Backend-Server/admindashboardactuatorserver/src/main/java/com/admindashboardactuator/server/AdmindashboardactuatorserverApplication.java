59e0e1f0d275068d2cad977ed9fca84db97eb462
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-05-14 11:55:53 +0200

    upload projects & first commit

---
+++ Backend-Server/admindashboardactuatorserver/src/main/java/com/admindashboardactuator/server/AdmindashboardactuatorserverApplication.java
@@ -0,0 +1,13 @@
+package com.admindashboardactuator.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmindashboardactuatorserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdmindashboardactuatorserverApplication.class, args);
+    }
+
+}
