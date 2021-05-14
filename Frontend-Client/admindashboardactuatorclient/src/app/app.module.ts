4d0859a71bb3f10f49f64a4b8c508391f5ea0b82
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-05-14 21:14:52 +0200

    generate new components (navbar,header,footer) & update app component

--- Frontend-Client/admindashboardactuatorclient/src/app/app.module.ts
+++ Frontend-Client/admindashboardactuatorclient/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavbarComponent,
+    MainHeaderComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
