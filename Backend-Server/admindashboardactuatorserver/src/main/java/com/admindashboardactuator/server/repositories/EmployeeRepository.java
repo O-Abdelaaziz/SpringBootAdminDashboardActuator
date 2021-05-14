25e3293a0e3325102639df204c58d9c8e1f3812d
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-05-14 15:24:14 +0200

    update employee repository (fix jpa method name)

--- Backend-Server/admindashboardactuatorserver/src/main/java/com/admindashboardactuator/server/repositories/EmployeeRepository.java
+++ Backend-Server/admindashboardactuatorserver/src/main/java/com/admindashboardactuator/server/repositories/EmployeeRepository.java
@@ -3,9 +3,10 @@ package com.admindashboardactuator.server.repositories;
 import com.admindashboardactuator.server.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-    List<Employee> findEmployeeByNameContaining(String name);
+    List<Employee> findEmployeeByFirstNameContaining(String name);
+    List<Employee> findEmployeeByLastNameContaining(String name);
 }
