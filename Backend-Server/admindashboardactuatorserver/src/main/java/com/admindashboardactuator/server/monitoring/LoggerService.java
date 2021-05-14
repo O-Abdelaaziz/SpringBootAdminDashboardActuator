0c81bf536a892e07a1e68f74ffccfd4e4c1c70b1
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-05-14 13:25:19 +0200

    add dummy service to check logger service under monitoring package

---
+++ Backend-Server/admindashboardactuatorserver/src/main/java/com/admindashboardactuator/server/monitoring/LoggerService.java
@@ -0,0 +1,22 @@
+package com.admindashboardactuator.server.monitoring;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoggerService implements HealthIndicator {
+
+    private final String Logger_SERVICE="Logger Service";
+    @Override
+    public Health health() {
+        if(isLoggerServiceGood()){
+            return Health.up().withDetail(Logger_SERVICE,"Logger service is running").build();
+        }
+        return Health.down().withDetail(Logger_SERVICE,"Logger service is not available").build();
+    }
+
+    private boolean isLoggerServiceGood(){
+        return false;
+    }
+}
